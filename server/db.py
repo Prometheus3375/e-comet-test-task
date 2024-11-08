8c5dd8da699d0e1257e9d987cf654229b3a484f7
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-11-08 22:49:53 +0300

    [server.db] Complete queries

--- server/db.py
+++ server/db.py
@@ -1,6 +1,6 @@
 import os
 from datetime import date
-from typing import final
+from typing import LiteralString, final
 
 from psycopg import AsyncConnection
 from psycopg.rows import DictRow, class_row
@@ -48,26 +48,83 @@ class PostgreSQLManager:
         conn = await cls.__connect()
         async with conn.cursor(row_factory=class_row(RepoDataWithRank)) as cursor:
             result = await cursor.execute(
-                '',
-                dict(n=n),
-                )  # todo
+                """
+                with current_places as (
+                    select *, rank() over (order by stars desc) as position_cur
+                    from repositories
+                    limit %(top_n)s
+                    )
+                select (owner || '/' || repo) as repo
+                     , owner
+                     , position_cur
+                     , place as position_prev
+                     , stars
+                     , watchers
+                     , forks
+                     , open_issues
+                     , language
+                
+                from current_places
+                    left join previous_places
+                    on current_places.id = previous_places.repo_id
+                """,
+                dict(top_n=n),
+                )
             return await result.fetchall()
+
+    _query_fetch_activity_all: LiteralString = """
+        with this_repo as (
+            select id, owner, repo
+            from repositories
+            where owner = %(owner)s and repo = %(repo)s
+            )
+        select date, commits, authors
+        from this_repo
+            left join activity
+            on this_repo.id = activity.repo_id
+        """
+    _query_fetch_activity_since: LiteralString = f"""
+        {_query_fetch_activity_all.lstrip()}
+        where %(since)s <= date
+        """
+    _query_fetch_activity_until: LiteralString = f"""
+        {_query_fetch_activity_all.lstrip()}
+        where date <= %(until)s
+        """
+    _query_fetch_activity_since_until: LiteralString = f"""
+        {_query_fetch_activity_all.lstrip()}
+        where %(since)s <= date and date <= %(until)s
+        """
 
     @classmethod
     async def fetch_activity(
             cls,
             /,
-            repo: str,
             owner: str,
-            since: date,
-            until: date,
+            repo: str,
+            since: date | None,
+            until: date | None,
             ) -> list[RepoActivity]:
         """
         Fetches the activity for a repository with the given owner for the specified period.
         """
+        query: LiteralString
+        if since is None and until is None:
+            query = cls._query_fetch_activity_all
+            params = dict(repo=repo, owner=owner)
+        elif since is None:
+            query = cls._query_fetch_activity_until
+            params = dict(repo=repo, owner=owner, until=until)
+        elif until is None:
+            query = cls._query_fetch_activity_since
+            params = dict(repo=repo, owner=owner, since=since)
+        else:
+            query = cls._query_fetch_activity_since_until
+            params = dict(repo=repo, owner=owner, since=since, until=until)
+
         conn = await cls.__connect()
         async with conn.cursor(row_factory=class_row(RepoActivity)) as cursor:
-            result = await cursor.execute('')  # todo
+            result = await cursor.execute(query, params)
             return await result.fetchall()
 
 
