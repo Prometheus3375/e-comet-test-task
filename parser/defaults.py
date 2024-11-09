3eaabf29f3ee497e13f6185201490700599fff42
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-11-09 20:13:54 +0300

    [parser] Add defaults

---
+++ parser/defaults.py
@@ -0,0 +1,9 @@
+DEFAULT_NEW_REPO_LIMIT = None
+"""
+A default value for the limit for new repositories added to the database.
+"""
+DEFAULT_AFTER_GITHUB_ID = 815_368_990
+"""
+A default value for GitHub repository ID after which new repositories are added to the database.
+This value is just before ID for Prometheus3375/dim-wishlist.
+"""
