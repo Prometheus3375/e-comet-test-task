248b7c2a2b33be15c6b579bbd046233a2121ac54
Prometheus3375 <35541026+Prometheus3375@users.noreply.github.com>
2024-11-11 00:52:11 +0300

    [parser] Add options to skip steps 1 and 2

--- parser/update.py
+++ parser/update.py
@@ -59,6 +59,8 @@ def update_database(
         database_uri: str,
         /,
         *,
+        skip_rank_update: bool,
+        skip_repo_update: bool,
         new_repo_limit: int | None,
         after_github_id: int,
         ) -> None:
@@ -66,11 +68,13 @@ def update_database(
     Updates the database with the new information about repositories and their activity.
 
     :param database_uri: URI of the database.
+    :param skip_rank_update: If ``True``, skips the step of updating table for previous places.
+    :param skip_repo_update: If ``True``, skips the step of updating already present repositories.
     :param new_repo_limit: The maximum number of new repositories to fetch from GitHub API.
         Can be ``None`` to fetch an unlimited number of repositories.
     :param after_github_id: Any new repository will have GitHub ID higher than this value.
     """
-    # Verify parameters
+    # region Verify parameters
     if new_repo_limit is None:
         new_repo_limit = inf
 
@@ -87,6 +91,7 @@ def update_database(
 
     # This value must be positive
     after_github_id = max(0, after_github_id)
+    # endregion
 
     conn: Connection[TupleRow]
     with Connection.connect(database_uri) as conn:
@@ -94,105 +99,111 @@ def update_database(
         # The latest top always evaluated on demand;
         # while the database updating, the top at the beginning of update will become obsolete
         # and will represent previous top.
-        logger.info('Step 1: update table \'previous_places\'')
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """
-                begin;
-                merge into previous_places
-                using (
-                    select id, rank() over (order by stars desc) as p
-                    from repositories
-                    ) as current_places
-                on current_places.id = previous_places.repo_id
-                -- Avoid updates if data is unchanged
-                when matched and place <> p then
-                    update set place = p
-                when not matched then  -- when not matched by target
-                    insert (repo_id, place)
-                    values (current_places.id, current_places.p);
-                commit;
-                """
-                )
+        if skip_rank_update:
+            logger.info('Step 1 skipped')
+        else:
+            logger.info('Step 1: update table \'previous_places\'')
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """
+                    begin;
+                    merge into previous_places
+                    using (
+                        select id, rank() over (order by stars desc) as p
+                        from repositories
+                        ) as current_places
+                    on current_places.id = previous_places.repo_id
+                    -- Avoid updates if data is unchanged
+                    when matched and place <> p then
+                        update set place = p
+                    when not matched then  -- when not matched by target
+                        insert (repo_id, place)
+                        values (current_places.id, current_places.p);
+                    commit;
+                    """
+                    )
 
-        logger.info('Step 1: complete')
+            logger.info('Step 1: complete')
 
         # Step 2: update existing repos
-        logger.info('Step 2: update existing repositories')
-        with conn.cursor() as existing_repos:
-            existing_repos.execute(
-                """
-                select id, owner, repo, last_activity_date
-                from repositories
-                left join
-                (
-                    select repo_id, max(date) as last_activity_date
-                    from activity
-                    group by repo_id
-                ) as dates
-                on repositories.id = dates.repo_id;
-                """
-                )
+        updated_repos = set()
+        if skip_repo_update:
+            logger.info('Step 2 skipped')
+        else:
+            logger.info('Step 2: update existing repositories')
+            with conn.cursor() as existing_repos:
+                existing_repos.execute(
+                    """
+                    select id, owner, repo, last_activity_date
+                    from repositories
+                    left join
+                    (
+                        select repo_id, max(date) as last_activity_date
+                        from activity
+                        group by repo_id
+                    ) as dates
+                    on repositories.id = dates.repo_id;
+                    """
+                    )
 
-            updated = set()
-            for repo_id, owner, repo, last_activity_date in existing_repos:
-                # Add to updated regardless of the request result
-                # as inserting an existing repo later will cause an error
-                full_name = f'{owner}/{repo}'
-                updated.add(full_name)
-                repo_data = request_repo(owner, repo)
-                if not repo_data: continue
+                for repo_id, owner, repo, last_activity_date in existing_repos:
+                    # Add to updated regardless of the request result
+                    # as inserting an existing repo later will cause an error
+                    full_name = f'{owner}/{repo}'
+                    updated_repos.add(full_name)
+                    repo_data = request_repo(owner, repo)
+                    if not repo_data: continue
 
-                # Update repository and activity
-                with conn.transaction():
-                    # Update repository
-                    with conn.cursor() as crs:
-                        crs.execute(
-                            """
-                            update repositories
-                            set stars = %(stars)s
-                                , watchers = %(watchers)s
-                                , forks = %(forks)s
-                                , open_issues = %(open_issues)s
-                                , language = %(language)s
-                            where 
-                                id = %(id)s
-                                -- Avoid updates if all values unchanged
-                                and (
-                                    stars <> %(stars)s
-                                    or watchers <> %(watchers)s
-                                    or forks <> %(forks)s
-                                    or open_issues <> %(open_issues)s
-                                    or language <> %(language)s
+                    # Update repository and activity
+                    with conn.transaction():
+                        # Update repository
+                        with conn.cursor() as crs:
+                            crs.execute(
+                                """
+                                update repositories
+                                set stars = %(stars)s
+                                    , watchers = %(watchers)s
+                                    , forks = %(forks)s
+                                    , open_issues = %(open_issues)s
+                                    , language = %(language)s
+                                where 
+                                    id = %(id)s
+                                    -- Avoid updates if all values unchanged
+                                    and (
+                                        stars <> %(stars)s
+                                        or watchers <> %(watchers)s
+                                        or forks <> %(forks)s
+                                        or open_issues <> %(open_issues)s
+                                        or language <> %(language)s
+                                    )
+                                """,
+                                dict(
+                                    id=repo_id,
+                                    stars=repo_data.stars,
+                                    watchers=repo_data.watchers,
+                                    forks=repo_data.forks,
+                                    open_issues=repo_data.open_issues,
+                                    language=repo_data.language,
+                                    ),
                                 )
-                            """,
-                            dict(
-                                id=repo_id,
-                                stars=repo_data.stars,
-                                watchers=repo_data.watchers,
-                                forks=repo_data.forks,
-                                open_issues=repo_data.open_issues,
-                                language=repo_data.language,
-                                ),
+
+                        # Update activity
+                        update_activity(
+                            conn,
+                            repo_id,
+                            owner=owner,
+                            repo=repo,
+                            last_activity_date=last_activity_date,
                             )
+                        logger.info(f'Updated repository {repo_id} {full_name!r}')
 
-                    # Update activity
-                    update_activity(
-                        conn,
-                        repo_id,
-                        owner=owner,
-                        repo=repo,
-                        last_activity_date=last_activity_date,
-                        )
-                    logger.info(f'Updated repository {repo_id} {full_name!r}')
-
-        logger.info('Step 2: complete')
+            logger.info('Step 2: complete')
 
         # Step 3: fetch new repositories
         logger.info(f'Step 3: fetch new repositories (up to {new_repo_limit})')
         it = request_public_repositories(
             new_repo_limit,
-            skip_repos=updated,
+            skip_repos=updated_repos,
             after_github_id=after_github_id,
             )
         for repo_data in it:
